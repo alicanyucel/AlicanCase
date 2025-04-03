841553739c6ea3801181d681a2cccd3bf40535d2
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-04-03 22:34:20 +0300

    dashboard

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Product from './product';
+import Dashboard from './Dashboard';
 
 const Stack = createStackNavigator();
-// burada ik sayfa olarak ekleme formu geelcek
 export default function App() {
   return (
     <NavigationContainer>
@@ -13,6 +13,11 @@ export default function App() {
           name="Product"
           component={Product}
           options={{ title: 'Ürün Kayıt Formu' }}
+        />
+        <Stack.Screen
+          name="Dashboard"
+          component={Dashboard}
+          options={{ title: 'Dashboard' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
